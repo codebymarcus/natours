3920701b4aeaa3ce53e1a5509fca84b63a56ebf1
marcus <marcus@appshouse.com>
2022-01-17 16:09:05 +0800

    ...

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DB_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB connected successfully!');
+  });
 
 const app = require('./app');
 
